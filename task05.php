e99089ab2659ae20e934840a6b7e0a3a980458e9
xRezio <edwin.carin@epitech.eu>
2023-10-04 16:46:20 +0200

    task06

--- task05.php
+++ task05.php
@@ -7,12 +7,9 @@ function calc_average(array $numbers) {
     $total = array_sum($numbers);
     $count = count($numbers);
 
-    if ($count === 0) {
-        return null; 
-    }
-
     $average = round($total / $count, 1); 
-
     return $average;
 }
+
+var_dump(calc_average([1, 4, 28, 12, 3, 4, 4, 27]));
 ?>
