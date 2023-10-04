3cc4a0ecb6e53472a7a1fdf35996ca58928a52fe
xRezio <edwin.carin@epitech.eu>
2023-10-04 13:53:23 +0200

    edit 01

--- task01.php
+++ task01.php
@@ -2,12 +2,10 @@
 
 function discover_type(int $age, string $name, float $gpa, bool $isStudent) {
     if ($isStudent) {
-        echo "Hello my name is $name, I'm $age years old. I'm a student at Epitech with a GPA of $gpa.";
+        echo "Hello my name is $name, I'm $age years old. I'm student at Epitech with a GPA of $gpa.";
     } else {
         echo "Hello my name is $name, I'm $age years old. I'm not a student.";
     }
 }
 
-
-discover_type($age, $name, $gpa, $isStudent);
 ?>
